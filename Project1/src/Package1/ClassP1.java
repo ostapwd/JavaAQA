6e3bb0af62be0ecbe0d04eb7ca02c135c7e3040f
Yaroslava Pasteruk <pasteruk.jaroslava@gmail.com>
2019-06-04 21:47:52 +0300

    new updates added

--- Project1/src/Package1/ClassP1.java
+++ Project1/src/Package1/ClassP1.java
@@ -3,6 +3,8 @@ package Package1;
 public class ClassP1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world, Yaroslava");
+		
+		System.out.println("Hello world, Yaroslava");
 	}
 
 }
