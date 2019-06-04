eac480bbae231166ce1128dc1dddce2588e9cb7c
OlenaHelenaElena <olena.diedyk@kindgeek.com>
2019-06-04 21:48:05 +0200

    new java project2

--- New java project/src/AddNewClass/NewMass.java
+++ New java project/src/AddNewClass/NewMass.java
@@ -3,6 +3,7 @@ package AddNewClass;
 public class NewMass {
 	public static void main(String[] args) {
 		System.out.println("Hello Olena, ldjaldjf");
+		System.out.println("Hello Olena, ldjaldjf");
 	}
 
 }
