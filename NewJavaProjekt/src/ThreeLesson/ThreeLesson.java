1ec4d01e3720ac23f8920e55f456b64a5f7c490b
User <you@example.com>
2019-06-04 21:46:47 +0300

    update projekt in nadiia

--- NewJavaProjekt/src/ThreeLesson/ThreeLesson.java
+++ NewJavaProjekt/src/ThreeLesson/ThreeLesson.java
@@ -1,9 +1,9 @@
 package ThreeLesson;
 
-
 public class ThreeLesson {
 
 	public static void main(String[] args) {
+		System.out.println("Hi =))))))))))))))))))");
 		System.out.println("Hi =))))))))))))))))))");
 	}
 
