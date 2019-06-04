4db670062b784458a6212cb22ddaed4df56c8ccd
ostapwdwd <ostapwdwd@gmail.com>
2019-06-04 22:48:49 +0300

    fff

---
+++ TaniaFirstProject/src/p1/MainClass.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class MainClass {
+	public static void main(String[] args) {
+		m1(7, 9);
+	}
+
+	public static int m1(String name, int a, int b) {
+		String name;
+		
+	}	
+}
