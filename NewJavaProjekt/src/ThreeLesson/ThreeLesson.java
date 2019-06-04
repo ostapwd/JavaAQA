921268e35cd85f51dfb146bb0510ca50af4315de
User <you@example.com>
2019-06-04 21:49:38 +0300

    blabla

--- NewJavaProjekt/src/ThreeLesson/ThreeLesson.java
+++ NewJavaProjekt/src/ThreeLesson/ThreeLesson.java
@@ -5,6 +5,7 @@ public class ThreeLesson {
 	public static void main(String[] args) {
 		System.out.println("Hi =))))))))))))))))))");
 		System.out.println("Hi =))))))))))))))))))");
+		System.out.println("Hi =))))))))))))))))))");
 	}
 
 }
