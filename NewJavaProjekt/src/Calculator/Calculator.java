107035bc3911d9e439e33ac2dfbf7536709ac887
User <you@example.com>
2019-06-08 00:10:55 +0300

    blabla

---
+++ NewJavaProjekt/src/Calculator/Calculator.java
@@ -0,0 +1,48 @@
+package Calculator;
+
+public class Calculator {
+	// INTEGER
+	public static int addition(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+
+	public static int subtract(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+
+	public static int multiplication(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+
+	public static int division(int a, int b) {
+		int result = a / b;
+		return result;
+	}
+
+	// DOUBLE
+	public static double addition(double a, double b) {
+		double result = a + b;
+		return result;
+	}
+
+	public static double subtract(double a, double b) {
+		double result = a - b;
+		return result;
+	}
+
+	public static double multiply(double a, double b) {
+		double result = a * b;
+		return result;
+	}
+
+	public static double division(double a, double b) {
+		double result = a / b;
+		return result;
+	}
+	
+	
+
+}
