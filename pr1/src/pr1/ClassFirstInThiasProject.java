f7f7733cc899f37ec29f46779fb6eb06c6d74eb1
ostapwdwd <ostapwdwd@gmail.com>
2019-06-08 10:42:22 +0300

    some chages

--- pr1/src/pr1/ClassFirstInThiasProject.java
+++ pr1/src/pr1/ClassFirstInThiasProject.java
@@ -3,5 +3,6 @@ package pr1;
 public class ClassFirstInThiasProject {
 	public static void main(String[] args) {
 		System.out.println("Hello from me, bla-bla-bla");
+		System.out.println("Hello from me, bla-bla-bla hhhhhhhh");
 	}
 }
