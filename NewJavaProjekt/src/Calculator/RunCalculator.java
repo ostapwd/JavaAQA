12ef390f4169e8169e0f50e466368c2ec5e7c9b7
User <you@example.com>
2019-06-11 14:39:30 +0300

    RunCalculator Update

--- NewJavaProjekt/src/Calculator/RunCalculator.java
+++ NewJavaProjekt/src/Calculator/RunCalculator.java
@@ -1,52 +1,41 @@
 package Calculator;
+
 import javax.swing.JOptionPane;
+
 public class RunCalculator {
 
 	public static void main(String[] args) {
+		boolean command3res;
+		do {
+			String command = JOptionPane.showInputDialog("Select a command:");
 
-//		int resAddI = Calculator.addition(2, 2);
-//		System.out.println(resAddI);
-//		int resSubI = Calculator.subtract(2, 2);
-//		System.out.println(resSubI);
-//		int resMultI = Calculator.multiplication(2, 2);
-//		System.out.println(resMultI);
-//		int resDivI = Calculator.division(2, 2);
-//		System.out.println(resDivI);
-//		double resAddD = Calculator.addition(1.3, 2.7);
-//		System.out.println(resAddD);
-//		double resSubD = Calculator.subtract(0.3, 2.1);
-//		System.out.println(resSubD);
-//		double resMultD = Calculator.multiply(3.1, 2.2);
-//		System.out.println(resMultD);
-//		double resDivD = Calculator.division(2.2, 1.1);
-//		System.out.println(resDivD);
+			if (!command.equals("/") && !command.equals("*") && !command.equals("-") && !command.equals("+")) {
+				String command2 = JOptionPane.showInputDialog(
+						"\"Error!!! Incorrect value is typed!!!\nCommand should contain +, -, *, /.\"");
+				break;
+			}
+			String number1 = JOptionPane.showInputDialog("Set the first number:");
 
-		String command = JOptionPane.showInputDialog("Select a command:");
-		//System.out.println(command);
-		
-		String number1 = JOptionPane.showInputDialog("Set the first number:");
-		//System.out.println(number1);
-		
-		String number2 = JOptionPane.showInputDialog("Set the second number:");
-		//System.out.println(number2);
-		 
-		double a = Double.parseDouble(number1);
-		double b = Double.parseDouble(number2);
-		
-		//System.out.println(a);
-		//System.out.println(b);
-		
-		if(command.equals("+")){
-			double result = Calculator.addition(a, b);
-		System.out.println(result);
-		}
-		if(command.equals("-")) {
-			double result = Calculator.subtract(a, b);
-			System.out.println(result);
-		}
-		
-		
-		// загнати у вайл поки юзер не введе While
-		
+			String number2 = JOptionPane.showInputDialog("Set the second number:");
+
+			double a = Double.parseDouble(number1);
+			double b = Double.parseDouble(number2);
+
+			if (command.equals("+")) {
+				double result = Calculator.addition(a, b);
+				System.out.println(result);
+			} else if (command.equals("-")) {
+				double result = Calculator.subtract(a, b);
+				System.out.println(result);
+			} else if (command.equals("*")) {
+				double result = Calculator.multiply(a, b);
+				System.out.println(result);
+			} else if (command.equals("/")) {
+				double result = Calculator.division(a, b);
+				System.out.println(result);
+			}
+           String command3 = JOptionPane.showInputDialog("Want to continue working with the calculator? (Y/N)");
+            command3res = command3.equals("Y");
+		} while (command3res);
 	}
 }
