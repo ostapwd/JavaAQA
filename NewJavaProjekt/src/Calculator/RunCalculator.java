c024eb3dd92f9edd39689250a7c2a5767b7fe1a2
User <you@example.com>
2019-06-12 14:02:28 +0300

    Updated calculator

--- NewJavaProjekt/src/Calculator/RunCalculator.java
+++ NewJavaProjekt/src/Calculator/RunCalculator.java
@@ -5,14 +5,16 @@ import javax.swing.JOptionPane;
 public class RunCalculator {
 
 	public static void main(String[] args) {
+
+		double[] arrayresult = new double[10];
+        int i = 0;
 		boolean command3res;
 		do {
 			String command = JOptionPane.showInputDialog("Select a command:");
 
 			if (!command.equals("/") && !command.equals("*") && !command.equals("-") && !command.equals("+")) {
 				String command2 = JOptionPane.showInputDialog(
-						"\"Error!!! Incorrect value is typed!!!\nCommand should contain +, -, *, /.\"");
-				break;
+						"\"Error!!! Incorrect value is typed!!!\nCommand should contain +, -, *, /.\n E - exit");
 			}
 			String number1 = JOptionPane.showInputDialog("Set the first number:");
 
@@ -20,22 +22,26 @@ public class RunCalculator {
 
 			double a = Double.parseDouble(number1);
 			double b = Double.parseDouble(number2);
-
+			double result;
 			if (command.equals("+")) {
-				double result = Calculator.addition(a, b);
+				result = Calculator.addition(a, b);
 				System.out.println(result);
 			} else if (command.equals("-")) {
-				double result = Calculator.subtract(a, b);
+				result = Calculator.subtract(a, b);
 				System.out.println(result);
 			} else if (command.equals("*")) {
-				double result = Calculator.multiply(a, b);
+				result = Calculator.multiply(a, b);
 				System.out.println(result);
-			} else if (command.equals("/")) {
-				double result = Calculator.division(a, b);
+			} else {
+				result = Calculator.division(a, b);
 				System.out.println(result);
 			}
-           String command3 = JOptionPane.showInputDialog("Want to continue working with the calculator? (Y/N)");
-            command3res = command3.equals("Y");
+			arrayresult[i] = result;
+			i++;
+			String command3 = JOptionPane.showInputDialog("Want to continue working with calculator? (Y/N)");
+			command3res = command3.equals("Y");
 		} while (command3res);
+		  
+		System.out.println(arrayresult[2]);
 	}
 }
